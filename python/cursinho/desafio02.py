9ebd1a295d05d6d65e8e1b7c5f14fe3d4374891d
Vitor Cunha <104070767+Vitorcalves@users.noreply.github.com>
2023-09-03 18:21:28 -0300

    exercicio de string

---
+++ python/cursinho/desafio02.py
@@ -0,0 +1,13 @@
+nome = 'carol'
+hobby = 'ouvir musica'
+
+print(f'ola sou a {nome} e gosto de {hobby}')
+
+b = 'ba'
+parte2 = 'nica'
+a = 'a'
+r = 'ri'
+parte1 = 'eletro'
+t = 'te'
+
+print(f'{b}{t}{r}{a} {parte1}{parte2}')
