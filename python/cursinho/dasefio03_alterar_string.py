59a5e2bb95f3da835d4486313eeaf0e3a4c39f50
Vitor Cunha <104070767+Vitorcalves@users.noreply.github.com>
2023-09-03 18:35:35 -0300

    modificacao de strings

---
+++ python/cursinho/dasefio03_alterar_string.py
@@ -0,0 +1,24 @@
+nome_curso = ' Edicao de Video '
+print(nome_curso.upper())
+# deixa as letras maiusculas
+print(nome_curso.lower())
+# deixa as letras minusculas
+print(nome_curso.strip())
+# remove os espacos em branco
+print(nome_curso.lstrip())
+# remove os espacos a esquerda
+print(nome_curso.rstrip())
+# remove os espacos a direita
+print(nome_curso.find('cao'))
+# mostra a posicao da palavra
+print(nome_curso.replace('Video', 'Audio'))
+# troca a primeira palavra pela segunda
+
+
+a = 'e'
+b = 'MELHOR'
+c = 'QUE'
+d = 'feito'
+e = 'perfeito'
+
+print(f'{a.upper()} {b.lower()} {d.upper()} {c.lower()} {e.upper()}')
